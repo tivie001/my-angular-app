3257142ba813308893a76d34d6f4a623c2143d25
Tyler Ivie <Tivie@DHICorp.com>
2021-08-26 16:57:59 -0600

    Changed file structure to be under either lessons or assignments.

--- src/app/docs/lessons/sec2/binding-comp.ts
+++ src/app/docs/lessons/sec2/binding-comp.ts
@@ -1,15 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'bind-comp',
     templateUrl: './binding.comp.html'
 })
-export class BindComp {
-    
+export class BindComp {    
     propertyBind = " <pre><code><p [innerText]='playerStatus'></p></code></pre>";
     eventBind = "<button (click)='onCreate()'>AddServer</button>"
     twoWayBind = `<input type='text' (input)='onUpdatePlayerName($event)'/>
 <p><strong>Player Name:</strong> {{ playerName }}</p>`;
+    customBind = `<app-player-element *ngFor="let player of playerElements"
+    [element]="player"></app-player-element>`;
+    element = `@Input() element: {type: string, name: string, jerseyNum: number};`
     opened = false;
     playerStatus = "Active";
     playerName = '';
